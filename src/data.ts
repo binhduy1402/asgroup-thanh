80c1b2216998c162ceaf7a72f1b675aae60eaf12
Nguyen Pham Binh Duy <binhduy140203@gmail.com>
2026-08-24 16:21:59 +0700

    update

--- src/data.ts
+++ src/data.ts
@@ -10,7 +10,7 @@ export const BRAND_INFO = {
   tagline: "Heritage & Grace",
   hotline: "0942 181 822 - Ms. Thanh",
   email: "sales6@asgroup.com.vn",
-  address: "Binh Trung District, Ho Chi Minh City, Vietnam",
+  address: "Binh Trung Ward, Ho Chi Minh City, Vietnam",
 };
 
 export const COMPANY_STATS = [
